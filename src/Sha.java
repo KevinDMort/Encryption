688a3f74b54747cf27206bd6be845951ea285628
Kevin D. Mortensen <kevi5221@edu.zealand.dk>
2023-10-12 13:21:08 +0200

    Initial commit

---
+++ src/Sha.java
@@ -0,0 +1,22 @@
+import java.security.*;
+
+public class Sha1 {
+
+
+    MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+    public String SHA_encrypt(String input)
+    {
+        byte[] hashBytes = md.digest(input.getBytes());
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hashBytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        String shaHash = hexString.toString();
+        return shaHash;
+    }
+
+
+    public Sha1() throws NoSuchAlgorithmException {
+    }
+}
